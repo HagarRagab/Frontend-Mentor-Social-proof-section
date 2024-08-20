8dba5c4af3244ae4d502dfbccf68871ad4b26e63
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2024-08-21 01:19:36 +0300

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'league-spartan': ['League Spartan', 'sans-serif']
+      },
+      colors: {
+        'dark-magenta': 'hsl(300, 43%, 22%)',
+        'light-grayish-magenta': 'hsl(300, 24%, 96%)',
+        'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
+        'soft-pink': 'hsl(333, 80%, 67%)',
+      },
+      backgroundImage: {
+        // 'desk-top': 'url(/assets/images/bg-pattern-top-desktop.svg)',
+        'desk-bottom': 'url(/assets/images/bg-pattern-bottom-desktop.svg)'
+      }
+    },
   },
   plugins: [],
 }
