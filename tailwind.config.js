b6dbda70edf208de6a23d4efd12e35771b2e19b2
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2024-08-21 14:00:17 +0300

    some edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,12 @@ module.exports = {
         'league-spartan': ['League Spartan', 'sans-serif']
       },
       colors: {
-        'dark-magenta': 'hsl(300, 43%, 22%)',
-        'light-grayish-magenta': 'hsl(300, 24%, 96%)',
-        'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
+        magenta: {
+          DEFAULT: 'hsl(300, 43%, 22%)',
+          light: 'hsl(300, 24%, 96%)',
+          dark: 'hsl(303, 10%, 53%)',
+        },
         'soft-pink': 'hsl(333, 80%, 67%)',
-      },
-      backgroundImage: {
-        // 'desk-top': 'url(/assets/images/bg-pattern-top-desktop.svg)',
-        'desk-bottom': 'url(/assets/images/bg-pattern-bottom-desktop.svg)'
       }
     },
   },
